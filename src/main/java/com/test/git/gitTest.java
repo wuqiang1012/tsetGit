9e9041b5212d1a1ddcf66d15882ff9774ab41438
wuqiang <498426581@qq.com>
2021-05-08 22:22:33 +0800

    hot-fix first commit

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -4,5 +4,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello GIT");
         System.out.println("hello GIT2");
+        System.out.println("hello GIT3");
     }
 }
