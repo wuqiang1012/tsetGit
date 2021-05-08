cbed139a8c353fb7564a3c635785e82c6badaf6a
wuqiang <498426581@qq.com>
2021-05-08 22:15:21 +0800

    second commit

--- src/main/java/com/test/git/gitTest.java
+++ src/main/java/com/test/git/gitTest.java
@@ -3,5 +3,6 @@ package com.test.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello GIT");
+        System.out.println("hello GIT2");
     }
 }
